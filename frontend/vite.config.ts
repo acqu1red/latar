f07d0e27bec6e5b747f919d185c4cf9a8e6ac14a
acqu1redin <acquired.way@gmail.com>
2025-09-28 13:57:36 +0700

    news11212112129900921212121121212121121212121

---
+++ frontend/vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/latar/' // Добавляем базовый путь для GitHub Pages
+})
